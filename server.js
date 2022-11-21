4fa53295d0288da5faa21b2a673e1c37b438d729
milandev128 <talent111dev@gmail.com>
2022-11-21 09:45:27 +0100

    update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/easy-angular/assets'));
+app.use(express.static(__dirname + '/dist/easy-angular'));
 app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname + '/dist/easy-angular/assets/index.html'));});
+res.sendFile(path.join(__dirname + '/dist/easy-angular/index.html'));});
 app.listen(process.env.PORT || 8080);
