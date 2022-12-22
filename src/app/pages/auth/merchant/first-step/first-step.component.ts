f23b9e69203221069ee2993fd544a12746410b00
spider man <113818953+milandev128@users.noreply.github.com>
2022-12-23 00:13:36 +0100

    upload image in signup page

--- src/app/pages/auth/merchant/first-step/first-step.component.ts
+++ src/app/pages/auth/merchant/first-step/first-step.component.ts
@@ -10,6 +10,9 @@ export class MFirstStepComponent implements OnInit {
 
   @Input() data: any;
   @Output() onNext:EventEmitter<any> = new EventEmitter<any>();
+  
+  public preview : string = '';
+  public currentFile : any = null;
 
   constructor(private store: Store) { }
 
@@ -19,5 +22,31 @@ export class MFirstStepComponent implements OnInit {
   onClickNext() {
     this.store.dispatch(setMerchantSignupStepIndex({stepIndex: this.data.stepIndex + 1}))
     this.onNext.emit('');
+  }
+
+  openDialog() {
+    document.getElementById('account_file')?.click();
+  }
+
+  selectFile(event: any): void {
+    this.preview = '';
+    const selectedFiles = event.target.files;
+  
+    if (selectedFiles) {
+      const file: File | null = selectedFiles.item(0);
+  
+      if (file) {
+        this.preview = '';
+        this.currentFile = file
+        const reader = new FileReader();
+  
+        reader.onload = (e: any) => {
+          console.log(e.target.result);
+          this.preview = e.target.result;
+        };
+  
+        reader.readAsDataURL(this.currentFile);
+      }
+    }
   }
 }
